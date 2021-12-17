0ab7c82b863d501011025cb4cc42a065f0f64b11
Dan Elis <danelis19@gmail.com>
2021-12-17 11:19:22 +0100

    data loading successfully from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  // fetches data and sets state which rerenders App
+  componentDidMount() {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      })
+  }
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    else {
+      return (
+        <div className="App">
+          Data has been loaded
+        </div>
+      );
+    }
+  }
 }
-
-export default App;
